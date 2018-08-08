179d24a154b4e4cc6a3efc734e3f130352530e0d
ChijeteCraft <42193095+ChijeteCraft@users.noreply.github.com>
2018-08-08 11:29:10 -0300

    Update creador.php

--- creador.php
+++ creador.php
@@ -3,6 +3,6 @@ try {
     $conexion = new PDO("mysql:host="db4free.net:3306";dbname="chijetecraft", "chijetecraft", "chijete456");
     return $conexion;
   } catch (PDOException $e) {
-    return false;
+    echo "error de conexion";
   }
 ?>
